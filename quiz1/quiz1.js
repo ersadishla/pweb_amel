17cb2be158e3eff50b96f825b77a54d9520cd7f3
serafina <57977401+amlpm@users.noreply.github.com>
2020-10-14 13:12:08 +0800

    Finishing
    FIXX Code

--- quiz1/quiz1.js
+++ quiz1/quiz1.js
@@ -1,18 +1,14 @@
 function openNav() {
-  document.getElementById("mySideNav").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+  document.getElementById('mySideNav').style.width = '300px';
+  document.getElementById("main").style.marginLeft = '300px';
 }
 
 function closeNav() {
-  document.getElementById("mySideNav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById('mySideNav').style.width = '0';
+  document.getElementById('main').style.marginLeft= '0';
 }
 
-$(function(){
-  //Increment element dari 1 - 4 berdasarkan ID (karna ada 4 kategori yang berupa a)
-  var str = '#elemen';
-
-  $(document).ready(function() {
+$(document).ready(function() {
     var i, stop;
     i = 1;
     //jumlah elemen (ada 4)
@@ -22,6 +18,4 @@ $(function(){
       if (i > stop) return;
       $('#elemen'+(i++)).toggleClass('bounce');
     }, 500)
-
   });
-});
